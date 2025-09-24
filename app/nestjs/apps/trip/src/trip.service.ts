574a0bbb5020957f4970825c09b6856d7271dac4
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-09-24 21:46:36 +0700

    feat: add class-validator and class-transformer for DTO validation

--- app/nestjs/apps/trip/src/trip.service.ts
+++ app/nestjs/apps/trip/src/trip.service.ts
@@ -1,6 +1,9 @@
+import { CreateTripDto } from '@libs/common/dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TripService {
   constructor() {}
+
+  public createTrip = async (createTripDto: CreateTripDto) => {};
 }
