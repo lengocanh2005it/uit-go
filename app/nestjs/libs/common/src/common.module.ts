2f351edfe527063790031b63ff9ff2175eb2badc
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-10-05 21:01:32 +0700

    fix: adjust kong.yml and user-service routes

--- app/nestjs/libs/common/src/common.module.ts
+++ app/nestjs/libs/common/src/common.module.ts
@@ -1,12 +1,24 @@
 import envConfig from '@libs/common/configs/env.config';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { CommonService } from './common.service';
 import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt_secret', ''),
+        signOptions: {
+          expiresIn: configService.get<string>('jwt_expiration_time', '120s'),
+        },
+      }),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [envConfig],
