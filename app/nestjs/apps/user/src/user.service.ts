dced8efe7fadf237023b9bd815a5180b9a84522d
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-10-06 23:32:16 +0700

    chore(docker): add Dockerfiles for each service and update docker compose config

--- app/nestjs/apps/user/src/user.service.ts
+++ app/nestjs/apps/user/src/user.service.ts
@@ -1,49 +1,49 @@
-import {
-  InjectRabbitMqService,
-  InjectRedisService,
-} from '@libs/common/decorators';
-import { RabbitMQService } from '@libs/common/rabbitmq/rabbitmq.service';
-import { RedisService } from '@libs/common/redis/redis.service';
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User, UserProfile } from './entities';
-import { Repository } from 'typeorm';
 import { CreateUserDto } from '@libs/common/dto/user/create-user.dto';
-import * as bcrypt from 'bcryptjs'
 import { LoginUserDto } from '@libs/common/dto/user/login-user.dto';
-import { JwtService } from '@nestjs/jwt'
 import { UpdateProfileDto } from '@libs/common/dto/user/update-profile.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
+import { Repository } from 'typeorm';
+import { User, UserProfile } from './entities';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRabbitMqService() private readonly rabbitMQService: RabbitMQService,
-    @InjectRedisService() private readonly redisService: RedisService,
     @InjectRepository(User) private readonly userRepo: Repository<User>,
-    @InjectRepository(UserProfile) private readonly profileRepo: Repository<UserProfile>,
-    private readonly jwtService: JwtService
-  ) { }
+    @InjectRepository(UserProfile)
+    private readonly profileRepo: Repository<UserProfile>,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   public register = async (createUserDto: CreateUserDto) => {
-    const { email, password, role } = createUserDto
-    const exists = await this.userRepo.findOne({ where: { email } })
-    if (exists) throw new BadRequestException('Email đã được đăng ký')
+    const { email, password, role } = createUserDto;
+    const exists = await this.userRepo.findOne({ where: { email } });
+    if (exists) throw new BadRequestException('Email has existed.');
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = this.userRepo.create({
       email: email,
       passwordHash: hashedPassword,
-      role: role
-    })
-    await this.userRepo.save(user)
+      role: role,
+    });
+    await this.userRepo.save(user);
 
-    const profile = this.profileRepo.create({ userId: user.id })
+    const profile = this.profileRepo.create({ userId: user.id });
     await this.profileRepo.save(profile);
-    return user
+    return user;
   };
 
   public login = async (loginUserDto: LoginUserDto) => {
-    const { email, password } = loginUserDto
+    const { email, password } = loginUserDto;
     const user = await this.userRepo.findOne({ where: { email } });
     if (!user) throw new UnauthorizedException('Invalid credentials');
 
@@ -58,19 +58,19 @@ export class UserService {
     };
 
     const token = await this.jwtService.signAsync(payload, {
-      expiresIn: '1h',
-      secret: process.env.JWT_SECRET,
+      expiresIn: this.configService.get<string>('jwt_expiration_time', '120s'),
+      secret: this.configService.get<string>('jwt_secret', ''),
     });
 
     return { accessToken: token };
-  }
+  };
 
   public getUser = (userId: string) => {
     return this.userRepo.findOne({
       where: { id: userId },
       relations: ['profile'],
     });
-  }
+  };
 
   async updateProfile(userId: string, updateProfileDto: UpdateProfileDto) {
     const profile = await this.profileRepo.findOne({ where: { userId } });
