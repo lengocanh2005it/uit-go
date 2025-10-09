7892b07cbe79a6eb6b3033617b9457536538d296
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-10-09 22:33:03 +0700

    feat: add paginated driver trip list

--- app/nestjs/apps/driver/src/driver.controller.ts
+++ app/nestjs/apps/driver/src/driver.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import type { TUserSession } from '@libs/common';
+import { UserSession } from '@libs/common/decorators';
+import { GetTripsOfDriverQueryDto } from '@libs/common/dto';
+import { Controller, Get, Param, ParseUUIDPipe, Query } from '@nestjs/common';
 import { DriverService } from './driver.service';
 
 @Controller('drivers')
 export class DriverController {
-  constructor(private readonly driverService: DriverService) { }
+  constructor(private readonly driverService: DriverService) {}
 
-  @Get('test')
-  test() {
-    return this.driverService.test();
+  @Get(':id/trips')
+  async getAllTripsOfDriver(
+    @UserSession() userSession: TUserSession,
+    @Param('id', ParseUUIDPipe)
+    driverId: string,
+    @Query() getTripsOfDriverQueryDto: GetTripsOfDriverQueryDto,
+  ) {
+    return this.driverService.getAllTripsOfDriver(
+      userSession,
+      driverId,
+      getTripsOfDriverQueryDto,
+    );
   }
 }
