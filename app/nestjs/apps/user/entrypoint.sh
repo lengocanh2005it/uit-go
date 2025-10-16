98b2ef20ae5222c1affce89400d2dd0b1c099f6e
Bao Le <levanbao2105@gmail.com>
2025-10-16 11:14:07 +0700

    fix: resolve conflicts in user shell script

--- app/nestjs/apps/user/entrypoint.sh
+++ app/nestjs/apps/user/entrypoint.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
-DOTENV_PRIVATE_KEY=$(tr -d '\r\n' </run/secrets/dotenv_user_key)
+DOTENV_PRIVATE_KEY=$(cat /run/secrets/dotenv_user_key | tr -d '\r\n')
+DOTENV_PRIVATE_KEY="$DOTENV_PRIVATE_KEY" npx dotenvx decrypt /apps/user/.env > /apps/user/.env.decrypted
 
-exec env DOTENV_PRIVATE_KEY="$DOTENV_PRIVATE_KEY" \
-  npx dotenvx run -- docker-entrypoint.sh postgres
+tr -d '\r' < /apps/user/.env.decrypted > /apps/user/.env.clean
+
+set -o allexport
+export $(grep -E '^[A-Za-z_][A-Za-z0-9_]*=' /apps/user/.env.clean | xargs)
+set +o allexport
+
+echo "✅ Decrypted and exported environment variables."
+
+exec gosu postgres docker-entrypoint.sh postgres
