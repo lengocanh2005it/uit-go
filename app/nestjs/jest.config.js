d3f88f5e5b46cb1488786311ff0b53e5cb6f2ea1
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-11-30 20:01:54 +0700

    ci: add GitHub Actions workflow

---
+++ app/nestjs/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+};
